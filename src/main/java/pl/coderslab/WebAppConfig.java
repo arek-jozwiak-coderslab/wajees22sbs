6dce20854c42418c18df244a652f6f697527a525
arek jozwiak <arkadiusz.jozwiak@coderslab.pl>
2021-12-14 10:28:49 +0100

    add resource image

--- src/main/java/pl/coderslab/WebAppConfig.java
+++ src/main/java/pl/coderslab/WebAppConfig.java
@@ -1,6 +1,7 @@
 package pl.coderslab;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,12 @@ public class WebAppConfig implements WebMvcConfigurer {
         registry.addViewController("/403").setViewName("403");
 //        registry.addViewController("/error").setViewName("404");
     }
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/", "file:/home/arek/Pictures/BOX/pudelka/");
+    }
+
 }
 
 
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -11,6 +11,8 @@
     <input class="fa fa-id-badge" type="submit" value="Wyloguj">
     <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
 </form>
+<!-- /home/arek/Pictures/BOX/pudelka/1-cute-funny-danbo-cardboard-box-art-lonely-hopscotch.jpg-->
+<img src="/resources/1-cute-funny-danbo-cardboard-box-art-lonely-hopscotch.jpg"/>
 
 <sec:authorize access="isAuthenticated()">
     Witaj  <sec:authentication property="principal.username" /> <br/>
